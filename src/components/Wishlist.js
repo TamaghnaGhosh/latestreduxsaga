e6d0242ef4b8a71044c563341e81e502aef9a440
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-03-25 18:20:41 +0530

    Fixed wishlist problem

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,55 +1,60 @@
-import React from 'react'
+import React from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { quentityAddToCart, wishListRmvFromCart } from "../redux/action";
-import Header from './Header';
+import Header from "./Header";
 
 const Wishlist = () => {
-    const keyIndex = new Date();
-    const dispatch = useDispatch();
-    const wishListData = useSelector((state) => state.wishListData);
+//   const keyIndex = new Date();
+  const dispatch = useDispatch();
+  const wishListData = useSelector((state) => state.wishListData);
 
-    return (
-        <div>
-        <Header/>
-            <NavLink className="NavLink" to="/">Go to Products Link</NavLink>
-            <h1>Wishlist</h1>
-            <div className="cart-page-container">
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Id</td>
-                            <td>Name</td>
-                            <td>Color</td>
-                            <td>Price</td>
-                            <td>Category</td>
-                            <td>Brand</td>
-                            <td>Button</td>
-                        </tr>
-                        {wishListData.map((item, index) => (
-                            <tr key={keyIndex.getSeconds()}>
-                                <td>{item.id}</td>
-                                <td>{item.name}</td>
-                                <td>{item.color}</td>
-                                <td>{item.price}</td>
-                                <td>{item.category}</td>
-                                <td>{item.brand}</td>
-                                <td>
-                                    <button className='' onClick={() => {
-                                        dispatch(wishListRmvFromCart(item.id));
-                                        dispatch(quentityAddToCart(item, "wish"))
-                                    }}>
-                                        move to cart
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <NavLink className="NavLink" to="/">
+        Go to Products Link
+      </NavLink>
+      <h1>Wishlist</h1>
+      <div className="cart-page-container">
+        <table>
+          <tbody>
+            <tr>
+              <td>Id</td>
+              <td>Name</td>
+              <td>Color</td>
+              <td>Price</td>
+              <td>Category</td>
+              <td>Brand</td>
+              <td>Button</td>
+            </tr>
+            {wishListData.map((item, index) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.color}</td>
+                <td>{item.price}</td>
+                <td>{item.category}</td>
+                <td>{item.brand}</td>
+                <td>
+                  <button
+                    className=""
+                    onClick={() => {
+                      dispatch(wishListRmvFromCart(item.id));
+                      dispatch(quentityAddToCart(item, "wish"));
+                    }}
+                  >
+                    move to cart
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default Wishlist
+export default Wishlist;
